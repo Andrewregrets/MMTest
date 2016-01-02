cc10e41dd2c3e9f24543329062d98c0ec572f086
Andrewregrets <andrewregrets@gmail.com>
2016-01-02 17:38:44 +0300

    Deleted comments.

--- app/src/main/java/com/music_math/mmtest/CategoriesActivity.java
+++ app/src/main/java/com/music_math/mmtest/CategoriesActivity.java
@@ -5,13 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.database.Cursor;
-import java.sql.SQLException;
 
 public class CategoriesActivity extends Activity implements View.OnClickListener{
     private static final String TABLE_NAME_TAG = "TableName";
